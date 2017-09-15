37f89bfe40e318472d3dced13bc380caab2f8fb5
ShraddhaMehta7 <30403528+ShraddhaMehta7@users.noreply.github.com>
2017-09-15 17:55:18 +0530

    Config file with Authentication details & endpoints

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config(object):
-    CLIENT_ID = 'hbgbVy5ORRWU2EgXFRnn5Q'
-    CLIENT_SECRET = 'RjeL5QH9u9ZmVa0holA1Dp8javUngDo9Ked58cIZr3hxa5haQ6C6IWMQob082uEW'
+    CLIENT_ID = 'your client id'
+    CLIENT_SECRET = 'your client secret'
     GRANT_TYPE = 'client_credentials'
-    BEARER_TOKEN = '_XNQQG71Z8n8_kDH61vWeBvcdWSJgzyaoxCS1xBoB9sg6KpvN3LGeMI9Mj3kTTD26A02CeUfQcBqo5sTCw-qD-z_sr6WjORqh45nwzptbzOjtbXagTk-8WvyXTSVWXYx'
+    
     API_HOST = 'https://api.yelp.com'
     SEARCH_PATH = '/v3/businesses/search'
     BUSINESS_PATH = '/v3/businesses/' 
